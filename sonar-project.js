10efd520834e860ed1d529befe8bb2ce9e0b18b8
MikeTecK <126035041+MikeTecK@users.noreply.github.com>
2023-04-10 00:42:11 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': 'admin',
+	    'sonar.login': '98dd302118b2663b6bc148ac112fab7fed4e517e',
 	    'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
